5735f790e2008bfe2407bcc8f7368c70653832ed
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2025-02-10 12:36:54 +0100

    add SSH VM injection

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/list/ListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/list/ListViewModel.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.kmpapp.screens.list
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jetbrains.kmpapp.data.MuseumObject
@@ -10,7 +11,10 @@ import kotlinx.coroutines.flow.stateIn
 import org.koin.android.annotation.KoinViewModel
 
 @KoinViewModel
-class ListViewModel(museumRepository: MuseumRepository) : ViewModel() {
+class ListViewModel(
+    val ssh : SavedStateHandle,
+    museumRepository: MuseumRepository
+) : ViewModel() {
     val objects: StateFlow<List<MuseumObject>> =
         museumRepository.getObjects()
             .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
